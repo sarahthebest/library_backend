3ba8042199e92d330a3b3387231b4c1ef4596f85
sarahthebest <sarah.emmoth@gmail.com>
2024-11-13 10:21:56 +0100

    Fixed User model

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
+const book_schema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     genre: [String],
@@ -14,4 +14,4 @@ const bookSchema = new mongoose.Schema({
     rating: { type: Number, min: 1, max: 5 },
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model("Book", book_schema);
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
+const user_schema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     UUID: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+module.exports = mongoose.model("User", user_schema);
