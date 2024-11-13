a5a9b605c1a239e312ca716164694d0dd4aa7c2a
sarahthebest <sarah.emmoth@gmail.com>
2024-11-13 09:37:22 +0100

    .

---
+++ src/models/Users.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    UUID: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
